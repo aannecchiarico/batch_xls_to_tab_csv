aa28f74c5e0701cf0f9e38e96d5fcf1e88d30e94
Aaron Annecchiarico <aannecchiarico@piedmontpays.com>
2017-03-09 21:12:36 -0500

    Add project files.

---
+++ batch_convert_excel/convert_form.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace batch_convert_excel
+{
+    partial class convert_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.filePath = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.process = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Folder Directory";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(16, 40);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(75, 23);
+            this.filePath.TabIndex = 1;
+            this.filePath.Text = "Browse";
+            this.filePath.UseVisualStyleBackColor = true;
+            this.filePath.Click += new System.EventHandler(this.getFilePath);
+            // 
+            // process
+            // 
+            this.process.Location = new System.Drawing.Point(16, 113);
+            this.process.Name = "process";
+            this.process.Size = new System.Drawing.Size(75, 23);
+            this.process.TabIndex = 2;
+            this.process.Text = "Process";
+            this.process.UseVisualStyleBackColor = true;
+            this.process.Click += new System.EventHandler(this.processForm);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Files Converted!";
+            // 
+            // convert_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.process);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.label1);
+            this.Name = "convert_form";
+            this.Text = "Batch Convert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button filePath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button process;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ batch_convert_excel/convert_form.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace batch_convert_excel
+{
+    
+    public partial class convert_form : Form
+    {
+        public string selectedPath;
+        public string[] fileEntries;
+        public const string csvExtension = ".csv";
+        public string csvFile; 
+        public convert_form()
+        {
+            InitializeComponent();
+            label2.Text = "";  
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        public void success() {
+            label2.ForeColor = Color.Green;
+            label2.Text = "Files converted!";
+        }
+
+        public void fail() {
+            label2.ForeColor = Color.Red;
+            label2.Text = "Failed to convert all files";
+        }
+       
+        private void getFilePath(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                selectedPath = folderBrowserDialog1.SelectedPath;
+                label1.Text = selectedPath;
+            }
+        }
+
+        private void processForm(object sender, EventArgs e)
+        {
+            fileEntries = Directory.GetFiles(selectedPath);
+
+            foreach (string fileName in fileEntries)
+            {
+                csvFile = Path.ChangeExtension(fileName, csvExtension);
+                ConvertExcelToCsv(fileName, csvFile);
+            }
+        }
+
+
+        public void ConvertExcelToCsv(string excelFilePath, string csvOutputFile, int worksheetNumber = 1)
+        {
+            if (!File.Exists(excelFilePath)) throw new FileNotFoundException(excelFilePath);
+            if (File.Exists(csvOutputFile)) throw new ArgumentException("File exists: " + csvOutputFile);
+
+            // connection string
+            var cnnStr = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=\"Excel 8.0;IMEX=1;HDR=NO\"", excelFilePath);
+            var cnn = new OleDbConnection(cnnStr);
+
+            // get schema, then data
+            var dt = new DataTable();
+            try
+            {
+                cnn.Open();
+                var schemaTable = cnn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                if (schemaTable.Rows.Count < worksheetNumber) throw new ArgumentException("The worksheet number provided cannot be found in the spreadsheet");
+                string worksheet = schemaTable.Rows[worksheetNumber - 1]["table_name"].ToString().Replace("'", "");
+                string sql = String.Format("select * from [{0}]", worksheet);
+                var da = new OleDbDataAdapter(sql, cnn);
+                da.Fill(dt);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // free resources
+                cnn.Close();
+            }
+
+            // write out CSV data
+            using (var wtr = new StreamWriter(csvOutputFile))
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    bool firstLine = true;
+                    foreach (DataColumn col in dt.Columns)
+                    {
+                        if (!firstLine) { wtr.Write("\t"); } else { firstLine = false; }
+                        var data = row[col.ColumnName].ToString().Replace("\"", "\"\"");
+                        wtr.Write(String.Format("\"{0}\"", data));
+                    }
+                    wtr.WriteLine();
+                }
+                success();
+            }
+        }
+        
+    }
+}
